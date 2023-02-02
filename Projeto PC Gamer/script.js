a217b857e6d3c872ecebfd141d0e2230df5517ee
Ivon Ferreira <ivonferreira@users.noreply.github.com>
2023-02-02 14:16:37 -0300

    Melhoras variaveis

--- Projeto PC Gamer/script.js
+++ Projeto PC Gamer/script.js
@@ -1,62 +1,13 @@
-let mem1 = document.querySelector('#mem1');
-let mem2 = document.querySelector('#mem2');
-let mem3 = document.querySelector('#mem3');
-let ssd = document.querySelector('#ssd');
-let cpuc = document.querySelector('#cpuc');
-let fans = document.querySelector('#fans');
-let cpu = document.querySelector('#cpu');
-let wifi = document.querySelector('#wifi');
-let comboAorus = document.querySelector('#comboAorus');
-let aorus = document.querySelector('#aorus');
-let comboAsus = document.querySelector('#comboAsus');
-let asus = document.querySelector('#asus');
-let mem1v = document.querySelector('#mem1v');
-let mem2v = document.querySelector('#mem2v');
-let mem3v = document.querySelector('#mem3v');
-let ssdv = document.querySelector('#ssdv');
-let cpucv = document.querySelector('#cpucv');
-let fansv = document.querySelector('#fansv');
-let cpuv = document.querySelector('#cpuv');
-let wifiv = document.querySelector('#wifiv');
-let comboAorusv = document.querySelector('#comboAorusv');
-let aorusv = document.querySelector('#aorusv');
-let comboAsusv = document.querySelector('#comboAsusv');
-let asusv = document.querySelector('#asusv');
+let campo = document.querySelectorAll(".campo");
+let valor = document.querySelectorAll(".valor");
 let total = document.querySelector('#total');
-let campos = [mem1,mem2,mem3,ssd,cpuc,fans,cpu,wifi,comboAorus,aorus,comboAsus,asus];
-let valores = [mem1v,mem2v,mem3v,ssdv,cpucv,fansv,cpuv,wifiv,comboAorusv,aorusv,comboAsusv,asusv];
+let comboAorus = document.querySelector('#comboAorus');
+let comboAsus = document.querySelector('#comboAsus');
+let aorus = document.querySelector('#aorus');
+let asus = document.querySelector('#asus');
+let wifi = document.querySelector('#wifi');
+let cpu = document.querySelector('#cpu');
 
-/*
-function memory(){
-    if (mem1.checked){
-        return mem1v.value;
-    }else if(mem2.checked){
-        return mem2v.value;
-    }else if (mem3.checked){
-        return mem3v.value;
-    }
-}
-function ssdf(){
-    if (ssd.checked){
-        return ssdv.value;
-    }else{
-        return 0
-    }
-}
-function cpucf(){
-    if (cpuc.checked){
-        return cpucv.value;
-    }else{
-        return 0
-    }
-}
-function fansf(){
-    if (fans.checked){
-        return fansv.value;
-    }else{
-        return 0
-    }
-}*/
 function combo(){
     if (comboAorus.checked || comboAsus.checked){
         cpu.checked = false;
@@ -80,9 +31,9 @@ document.querySelector('#calc').addEventListener('click',()=>{
     nowifi();
     wwifi();
     let sum = 0
-    for(let i in valores){
-        if (campos[i].checked){
-            sum += Number(valores[i].value);
+    for(let i in valor){
+        if (campo[i].checked){
+            sum += Number(valor[i].value);
             
         }
     }
